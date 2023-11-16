bf7c6e489899c4fa9a835b405d962f35e4d6db30
cp949 <cp949@naver.com>
2023-11-17 05:37:22 +0900

    update build scripts

--- very-basic-example/main/next.config.js
+++ very-basic-example/main/next.config.js
@@ -1,7 +1,15 @@
-module.exports = {
+const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
   reactStrictMode: false,
-  output: 'standalone',
+  output: BASE_PATH === '/' ? 'standalone' : 'export',
+  basePath: BASE_PATH === '/' ? '' : BASE_PATH,
   experimental: {
     webpackBuildWorker: true,
   },
 }
+
+module.exports = nextConfig
