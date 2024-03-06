44819f94a72102dd1102c53e1e6bec2456d6b4a1
seunghyeok.lee@kt.com <seunghyeok.lee@kt.com>
2024-03-06 15:48:53 +0900

    pingping 2,3,4 foleder delete, 1 folder name update

--- PingPongG4-ble/_export.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-cd "${SCRIPT_DIR}"
-
-pnpm install
-pnpm build
--- PingPongG4-ble/main/src/log.ts
+++
@@ -1,6 +0,0 @@
-import minilog, { enable } from 'minilog'
-import { HW_ID } from './constant'
-
-enable()
-
-export default minilog(HW_ID)
