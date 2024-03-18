13bee30f8f57d5642e7acda9160e4a3f971d7db7
Roborisen <dev@roborisen.com>
2024-03-18 16:55:49 +0900

    [Roborisen] 이미지 변경
    이미지 변경, blockcoding 블럭 코드 추가, G1 주석 수정

---
+++ pingpong-ble/blockcoding/g1_servo_set_degree.js
@@ -0,0 +1,5 @@
+javascriptGenerator.forBlock['oz71_g1_servo_set_degree'] = function(block) {
+  const number_distance = block.getFieldValue('DISTANCE');
+  const code = `await makerKit.hw.run(routineContext, "pingpong.setServoDegree", ${number_distance});\n`
+  return code;
+};
